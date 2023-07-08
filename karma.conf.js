62663b1a72182b9d7fc9c7256b516218d04a67d0
BRUNO-RAMIREZ <brunaisdiaz1@gmail.com>
2023-07-08 17:33:47 -0400

    initial commit 2

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/Project-Ang-One'),
+      dir: require('path').join(__dirname, './coverage/docs'),
       subdir: '.',
       reporters: [
         { type: 'html' },
